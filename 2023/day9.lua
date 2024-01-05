4807e5d808f1ed45b19cfc5499768fe8d9b0c659
wingly <winglyman@gmail.com>
2024-01-05 08:41:03 +0100

    day9, better late than never

--- 2023/day9.lua
+++ 2023/day9.lua
@@ -1,33 +1,75 @@
 require "utility"
 
-local input = read_lines('testinput')
-
-function generateLayers(layers)
-    table.insert(layers, {"os"})
-
+local input = read_lines('input')
+ 
+function isAllZeroes(layer)
+    local allZeroes = true
+    for _, num in ipairs(layer) do
+        if num ~= 0 then
+            allZeroes = false
+            break
+        end
+    end
+    return allZeroes
 end
 
-function part1(data)
-    printArray(data)
-    for _, row in ipairs(data) do
-        local layers = {
-        }
-        for num in string.gmatch(row, '(%d+)') do
-            if layers[1] == nil then
-                layers[1] = {num}
-            else
-                table.insert(layers[1], num)
-            end
-        end
-        print2DArray(layers)
-        generateLayers(layers)
-        print2DArray(layers)
+function generateLayers(layers)
+    if isAllZeroes(layers[#layers]) then
+        return
+    end
+    local newLayer = {}
+    local lastLayer = layers[#layers]
+    for i=1, #lastLayer-1 do
+        table.insert(newLayer, lastLayer[i+1] - lastLayer[i])
+    end
+    table.insert(layers, newLayer)
 
+    generateLayers(layers)
+end
+
+function parseData(row) do
+    local layers = {}
+    for num in string.gmatch(row, '(-?%d+)') do
+        if layers[1] == nil then
+            layers[1] = {tonumber(num)}
+        else
+            table.insert(layers[1], tonumber(num))
+        end
+    end
+    return layers
     end
 end
 
+
+function part1(data)
+    local retVal = 0
+    for _, row in ipairs(data) do
+        local layers = parseData(row)
+        generateLayers(layers)
+        for i=1, #layers-1 do
+            local prevLayer = layers[#layers - i+1]
+            local curLayer = layers[#layers - i]
+            table.insert(curLayer, curLayer[#curLayer] + prevLayer[#prevLayer])
+        end
+
+        retVal = retVal + layers[1][#layers[1]]
+    end
+    return retVal
+end
+
 function part2(data)
-  
+    local retVal = 0
+    for _, row in ipairs(data) do
+        local layers = parseData(row)
+        generateLayers(layers)
+        for i=1, #layers-1 do
+            local prevLayer = layers[#layers - i+1]
+            local curLayer = layers[#layers - i]
+            table.insert(curLayer, 1, curLayer[1] - prevLayer[1])
+        end
+        retVal = retVal + layers[1][1]
+    end
+    return retVal
 end
 
 print(part1(input))
